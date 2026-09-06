4c432502f3edd0d7edd5d2b8912fbb9af7b8d407
aadithyan <aadithyankanagaraj@gmail.com>
2026-09-06 23:32:40 +0530

    Day 1 - Contains Duplicate

---
+++ ARRAY/DAY1/ContainsDuplicate.java
@@ -0,0 +1,25 @@
+public class ContainsDuplicate {
+    public static void main(String args[])
+    {
+        int arr[]={1,23,34,65,56,1};
+        boolean duplicate=false;
+        for(int i=0;i<arr.length;i++)
+            {
+                for(int j=i+1;j<arr.length;j++)
+                    {
+                        if(arr[i]==arr[j])
+                        {
+                            duplicate=true;
+                          
+                            break;
+                        }
+                       
+
+                    }
+                
+            }
+            System.out.print(duplicate);
+        
+    }
+    
+}
